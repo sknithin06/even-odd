432c505bf0a27e83304910c281c9eaebc282c876
sknithin06 <nithinsk939@gmail.com>
2021-07-03 21:09:26 +0530

    even is added

--- evenodd.java
+++ evenodd.java
@@ -1,7 +1,18 @@
 package Java;
-
+import java.util.Scanner;
 public class evenodd {
 	public static void main(String[] args) {
 		System.out.println("Welcome to BasicCoreProgram");
+		
+		Scanner sc= new Scanner(System.in);   
+		System.out.print("Enter a value of a-");  
+		double value = sc.nextDouble();
+		double num = value % 2;
+		if (num == 0) {
+			System.out.println("Number is even:"+value);
+		}
+		else {
+			System.out.println("Number is odd:"+value);
+		}		
 	}
 }
